851d9608f38568e583de0119430e586bcd8040a4
GeorgeGithiri5 <georgegithiri002@gmail.com>
2023-01-31 09:03:23 +0300

    Added Helpers For Dags

---
+++ airflow/plugins/helpers/analytics_queries.py
@@ -0,0 +1,10 @@
+class AnalyticsQueries:
+    
+    create_schema = """CREATE SCHEMA IF NOT EXISTS goodreads_analytics;"""
+    
+    create_author_reviews = """
+        CREATE TABLE IF NOT EXISTS goodreads_analytics.popular_author_review_count
+        (
+            
+        )
+    """
